35f1b524e87749d8f84267bcefdf6227fadf9c34
parth22 <parthb7200@gmail.com>
2024-01-27 08:34:30 +0530

    Task 5 added

--- practical_5/lib/screen5.dart
+++ practical_5/lib/screen5.dart
@@ -3,13 +3,17 @@ import 'package:flutter/material.dart';
 class Screen5 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Task-5")),
+      appBar: AppBar(
+        title: Text("Task-5"),
+        centerTitle: true,
+      ),
       body: Center(
         child: Container(
           height: 200,
           width: 200,
-          color: Color.fromARGB(255, 255, 0, 0),
-          child: BorderRadius.circular(20.0),
+          // color: Color.fromARGB(255, 255, 0, 0),
+          decoration: BoxDecoration(
+              color: Colors.amber, borderRadius: BorderRadius.circular(70)),
         ),
       ),
     );
