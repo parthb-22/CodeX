3d29c95e916c40e1714cb9518a2891c237ecadef
parth22 <parthb7200@gmail.com>
2024-02-27 12:21:38 +0530

    customization

---
+++ codex_app_main/README.md
@@ -0,0 +1,3 @@
+# codex_app_main
+
+A new Flutter project.
---
+++ codex_app_main/android/app/src/main/kotlin/com/example/codex_app_main/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.codex_app_main
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
